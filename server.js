5a8f66163003362a2a4894681adb147079ab9f29
oliverhogstrom <54627509+oliverhogstrom@users.noreply.github.com>
2021-02-04 11:01:10 +0100

    Redone the whole thing :smiley:

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-const express = require('express')
-const app = express()
-const port = 3000
+// const express = require('express')
+// const app = express()
+// const port = 3000
 
-app.use(express.static(__dirname + '\\staticFiles\\'))
-app.use(express.json())
-app.use(express.urlencoded())
+// app.use(express.static(__dirname + '\\staticFiles\\'))
+// app.use(express.json())
+// app.use(express.urlencoded())
 
-app.post('/test', function (req, res) {
+// app.post('/test', function (req, res) {
 
-    console.log(req.body.kanelbulle);
+//     console.log(req.body.kanelbulle);
 
-    res.sendStatus(418)
-})
+//     res.sendStatus(418)
+// })
 
-app.listen(port, () => console.log(`Example app listening on port port!`))
+// app.listen(port, () => console.log(`Example app listening on port port!`))
