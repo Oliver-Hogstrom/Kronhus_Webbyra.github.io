859a98315dbe1357383ad28d72348821eb5f5e9b
oliverhogstrom <54627509+oliverhogstrom@users.noreply.github.com>
2021-03-03 11:47:49 +0100

    I want to die :skull:

--- server.js
+++ server.js
@@ -2,12 +2,38 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const clientDir = __dirname + "\\client\\"
+const htmlDir = __dirname + "\\html\\"
+const bsDir = __dirname + "\\bs\\"
+const styleDir = __dirname + "\\css\\"
 
-
-app.use(express.static(__dirname + '\\staticFiles\\'))
+app.use(express.static(clientDir))
 app.use(express.json())
 app.use(express.urlencoded())
 
+app.get('/', (req, res) => {
+    res.sendFile(htmlDir + 'index.html')
+})
+app.get('/service', (req, res) => {
+    res.sendFile(htmlDir + 'service.html')
+})
+app.get('/kontakt', (req, res) => {
+    res.sendFile(htmlDir + 'kontakt.html')
+})
+app.get('/Tjänster', (req, res) => {
+    res.sendFile(htmlDir + 'tjänser.html')
+})
+app.get('/Vilka är vi', (req, res) => {
+    res.sendFile(htmlDir + 'vilka-ar-vi.html')
+})
+
+// For the CSS and bootstrap files
+app.get('/style', (req, res) => {
+    res.sendFile(styleDir + 'main.css')
+})
+app.get('/bootstrap', (req, res) => {
+    res.sendFile(bsDir + 'bootstrap.min.css')
+})
 
 
 app.listen(port, () => console.log(`Example app listening on port port!`))
