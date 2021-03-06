e5cccea5f8cf9ade5653b6538961091cfd4d46a5
oliverhogstrom <oliver.hogstrom@gmail.com>
2021-03-06 16:46:03 +0100

    Will i have styling now?

--- server.js
+++ server.js
@@ -1,40 +1,40 @@
-const express = require('express')
+// const express = require('express')
 
-const app = express()
-const port = 3000
+// const app = express()
+// const port = 3000
 
-const clientDir = __dirname + "\\client\\"
-const docsDir = __dirname + "\\docs\\"
-const bsDir = __dirname + "\\bs\\"
-const styleDir = __dirname + "\\css\\"
+// const clientDir = __dirname + "\\client\\"
+// const docsDir = __dirname + "\\docs\\"
+// const bsDir = __dirname + "\\bs\\"
+// const styleDir = __dirname + "\\css\\"
 
-app.use(express.static(clientDir))
-app.use(express.json())
-app.use(express.urlencoded())
+// app.use(express.static(clientDir))
+// app.use(express.json())
+// app.use(express.urlencoded())
 
-app.get('/', (req, res) => {
-    res.sendFile(docsDir + 'index.html')
-})
-app.get('/service', (req, res) => {
-    res.sendFile(docsDir + 'service.html')
-})
-app.get('/kontakt', (req, res) => {
-    res.sendFile(docsDir + 'kontakt.html')
-})
-app.get('/Tjanster', (req, res) => {
-    res.sendFile(docsDir + 'tjänser.html')
-})
-app.get('/Vilka', (req, res) => {
-    res.sendFile(docsDir + 'vilka.html')
-})
+// app.get('/', (req, res) => {
+//     res.sendFile(docsDir + 'index.html')
+// })
+// app.get('/service', (req, res) => {
+//     res.sendFile(docsDir + 'service.html')
+// })
+// app.get('/kontakt', (req, res) => {
+//     res.sendFile(docsDir + 'kontakt.html')
+// })
+// app.get('/Tjanster', (req, res) => {
+//     res.sendFile(docsDir + 'tjänser.html')
+// })
+// app.get('/Vilka', (req, res) => {
+//     res.sendFile(docsDir + 'vilka.html')
+// })
 
-// For the CSS and bootstrap files
-app.get('/style', (req, res) => {
-    res.sendFile(styleDir + 'main.css')
-})
-app.get('/bootstrap', (req, res) => {
-    res.sendFile(bsDir + 'bootstrap.min.css')
-})
+// // For the CSS and bootstrap files
+// app.get('/style', (req, res) => {
+//     res.sendFile(styleDir + 'main.css')
+// })
+// app.get('/bootstrap', (req, res) => {
+//     res.sendFile(bsDir + 'bootstrap.min.css')
+// })
 
 
-app.listen(port, () => console.log(`Example app listening on port port!`))
+// app.listen(port, () => console.log(`Example app listening on port port!`))
