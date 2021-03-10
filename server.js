58e9fd4a43577b2b3eadedc69fa4b2dc63587f2d
oliverhogstrom <oliver.hogstrom@gmail.com>
2021-03-10 20:17:21 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const express = require('express')
 const app = express()
 const port = 3001
 
-const clientDir = __dirname + "//client//"
-const htmlDir = __dirname + "//html//"
-const bsDir = __dirname + "//bs//"
-const styleDir = __dirname + "//css//"
+const clientDir = __dirname + "/client/"
+const htmlDir = __dirname + "/html/"
+const bsDir = __dirname + "/bs/"
+const styleDir = __dirname + "/css/"
 
 app.use(express.static(clientDir))
 app.use(express.json())
@@ -37,4 +37,4 @@ app.get('/bootstrap', (req, res) => {
 })
 
 
-app.listen(port, () => console.log(`Example app listening on port port!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}`))
